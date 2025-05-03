571c0786ed0bf29b6a8d87981c2a56248dada687
Thomas <thomas@halbritter.org>
2025-05-03 10:23:44 +0200

    Statische Konstanten ergänzt (Abstände 'em')

--- CoreTextTableExample/MarkdownParser/MarkdownParser+Statics.swift
+++ CoreTextTableExample/MarkdownParser/MarkdownParser+Statics.swift
@@ -12,30 +12,38 @@ import UIKit
 
 public struct Markdown {
     
-    static let _1cm          : CGFloat = 1 / 2.54 * 72         /// 1cm in Zoll umrechnen und 72dpi Standard
-    static var useSoftBreaks : Bool = true                     /// Soft Breaks aktivieren / deaktivieren
+    static let _1cm          : CGFloat = 1 / 2.54 * 72       /// 1cm in Zoll umrechnen und 72dpi Standard
+    static var useSoftBreaks : Bool = true                   /// Soft Breaks aktivieren / deaktivieren
     
+    /// Abstände und Einzüge in 'em' als Multiplikator zum Text Size
+    static var lineHeightMultiple     : CGFloat = 1.1
+    static var paragraphSpacing       : CGFloat = 0.5
+    static var paragraphSpacingBefore : CGFloat = 1.2        /// Abstand vor einem Header
+    static var paddingHorz            : CGFloat = 0.5
+    static var paddingBefore          : CGFloat = 0.3
+    static var paddingAfter           : CGFloat = 0.3
+
     /// Konstanten für die Tabelle
     struct Table {
-        static var weightText   : UIFont.Weight = .regular     /// Stil des Textes in der Tabelle
-        static var weightHeader : UIFont.Weight = .bold        /// Stil der Überschrift in der Tabelle
-        static var weightBox    : UIFont.Weight = .ultraLight  /// Stil der Balken in der Tabelle
-        static var colorBox     : UIColor?      = nil          /// Farbe der Balken der Tabelle
+        static var weightText  : UIFont.Weight = .regular    /// Stil des Textes in der Tabelle
+        static var weightHeader: UIFont.Weight = .bold       /// Stil der Überschrift in der Tabelle
+        static var weightBox   : UIFont.Weight = .ultraLight /// Stil der Balken in der Tabelle
+        static var colorBox    : UIColor?      = nil         /// Farbe der Balken der Tabelle
     }
     
     /// Konstanten für die Liste (Anführungszeichen der Liste in der Hierarchie)
     struct List {
-        static var bulletPoint        : [String] = [.listBullet_1, .listBullet_2, .listBullet_3]
-        static var leftIndent         : CGFloat = 10           /// Linker Rand der untersten Hierarchie
+        static var bulletPoint      : [String] = [.listBullet_1, .listBullet_2, .listBullet_3]
+        static var leftIndent       : CGFloat = 10           /// Linker Rand der untersten Hierarchie
     }
     
     /// Konstanten für den vertikalen Trennstrich (Ruler)
     struct Ruler {
-        static var rightIndent        : CGFloat = 5            /// rechter Rand der Trennlinie
-        static var height             : CGFloat = 20           /// Höhe des Hintergrundes der Trennlinie
-        static var lineHeight         : CGFloat = 4            /// Strichdicke der Trennlinie
-        static var colorHighLight     : Bool    = true         /// Die Farbe der Trennlinie wird etwas heller
-    }                                                          /// als die Textfarbe dargestellt
+        static var rightIndent      : CGFloat = 5            /// rechter Rand der Trennlinie
+        static var height           : CGFloat = 20           /// Höhe des Hintergrundes der Trennlinie
+        static var lineHeight       : CGFloat = 4            /// Strichdicke der Trennlinie
+        static var colorHighLight   : Bool    = true         /// Die Farbe der Trennlinie wird etwas heller
+    }                                                        /// als die Textfarbe dargestellt
     
     /// Konstanten für den Hintergrund des Block Quote
     struct BlockQuote {
@@ -44,22 +52,14 @@ public struct Markdown {
         static var verticalOffset   : CGFloat = 5            /// Verschiebung des  Hintergrunds nach unten
         static var backgroundColor  : UIColor = .systemGray6 /// Farbe für den Hintergrund
         
-        static var barIndent     : CGFloat = 5            /// Linker Rand des Balkens
-        static var barWidth      : CGFloat = 10           /// Breite des Balkens
-        static var barColor      : UIColor = .systemGray4 /// Farbe für den Balken
+        static var barIndent        : CGFloat = 5            /// Linker Rand des Balkens
+        static var barWidth         : CGFloat = 10           /// Breite des Balkens
+        static var barColor         : UIColor = .systemGray4 /// Farbe für den Balken
     }
     
     /// Konstanten für den Code Block
     struct CodeBlock {
-        static var textsize       : CGFloat = 14           /// Größe des Textfonts im Code Block
-    }
-    
-    /// Konstanten für den  Header
-    struct Header {                                        /// Gerätespezifische Fontgrößen für  Header
-        static var fontSizes: [UIUserInterfaceIdiom : [CGFloat]] =
-        [.mac:   [34, 30, 24, 20, 18, 16],
-         .pad:   [32, 24, 20, 18, 16, 16],
-         .phone: [24, 18, 16, 16, 16, 16]]
+        static var textsize         : CGFloat = 14           /// Größe des Textfonts im Code Block
     }
 }
 
